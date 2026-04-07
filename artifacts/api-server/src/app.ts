87451ff4fec6a151969ed3d077c7c8e5fd6fb4ef
crosik21 <rouleroule2108@gmail.com>
2026-04-08 00:08:03 +0500

    Fix: use app.use instead of app.get('*') for Express 5 compatibility

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -16,7 +16,7 @@ app.use("/api", router);
 
 const frontendPath = path.resolve(__dirname, "../../mini-farm/dist/public");
 app.use(express.static(frontendPath));
-app.get("*", (_req, res) => {
+app.use((_req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
